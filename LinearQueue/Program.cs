4c779952db8179a195547e53e02ea5ae1c6b5dce
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2020-01-09 12:54:44 -0500

    Finishe Driver program

--- LinearQueue/Program.cs
+++ LinearQueue/Program.cs
@@ -10,6 +10,20 @@ namespace LinearQueue
     {
         static void Main(string[] args)
         {
+            Queue Que = new Queue(10);
+
+            Random ran = new Random();
+
+            
+
+            for(int i = 0; i <= 9; i++)
+            {
+                int num = ran.Next(50);
+                Que.enqueue(num);
+            }
+
+            Que.printQueue();
+            Que.dequeue();
         }
     }
 }
