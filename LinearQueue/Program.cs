7df285a8c33b9c55e69d468e1778002a63a1e235
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2020-01-09 12:57:25 -0500

    Added commets

--- LinearQueue/Program.cs
+++ LinearQueue/Program.cs
@@ -10,19 +10,23 @@ namespace LinearQueue
     {
         static void Main(string[] args)
         {
+            //Creates Queue
             Queue Que = new Queue(10);
 
+            //Creates Random 
             Random ran = new Random();
-
-            
 
             for(int i = 0; i <= 9; i++)
             {
+                //Assigins Random Number to num
                 int num = ran.Next(50);
-                Que.enqueue(num);
+                Que.enqueue(num); //Populates Queue
             }
 
+            //Prints Queue
             Que.printQueue();
+
+            //Removes First Item
             Que.dequeue();
         }
     }
